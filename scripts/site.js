dbc32503077102de400c55caf5094b1b160886eb
Anthony Jackman <energeticpixels@gmail.com>
2018-05-07 21:07:12 -0500

    control error msg depend on resource data

--- scripts/site.js
+++ scripts/site.js
@@ -1,16 +1,21 @@
 new Vue({
     el: '#simpleApp',
     data: {
-        showAlert: false
+        dataLoaded: 0,
+        apiData: undefined
     },
     methods: {
         loadApi: function() {
+            this.dataLoaded = 0;
             this.$http.get('https://jsonplaceholder.typicode.com/posts').then(this.successCallback, this.errorCallback);
         },
         successCallback: function(response) {
+            this.dataLoaded = 1;
             console.log('successCallback response: ', response);
+            this.apiData = response.data;
         },
         errorCallback: function(response) {
+            this.dataLoaded = 2;
             console.log('errorCallback response: ', response);
         }
     }
