5612f3c7b56faa018728b62014784f7e7a9cadc6
Anthony Jackman <energeticpixels@gmail.com>
2018-05-07 21:15:40 -0500

    load real external data

--- scripts/site.js
+++ scripts/site.js
@@ -1,21 +1,18 @@
 new Vue({
     el: '#simpleApp',
     data: {
-        dataLoaded: 0,
         apiData: undefined
     },
     methods: {
         loadApi: function() {
-            this.dataLoaded = 0;
-            this.$http.get('https://jsonplaceholder.typicode.com/posts').then(this.successCallback, this.errorCallback);
+            this.$http.get('data/data.json').then(this.successCallback, this.errorCallback);
         },
         successCallback: function(response) {
-            this.dataLoaded = 1;
-            console.log('successCallback response: ', response);
             this.apiData = response.data;
+            console.log('successCallback this.apiData: ', this.apiData);
+
         },
         errorCallback: function(response) {
-            this.dataLoaded = 2;
             console.log('errorCallback response: ', response);
         }
     }
